6c8d6b8dc7ffdb609a0da12970892d4faeb9ee53
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-11-20 02:59:04 +0300

    Update dashboardRoutes.js

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getDashboardPage, createOrder } = require('../controllers/dashboardController'); // <-- إضافة createOrder
+const dashboardController = require('../controllers/dashboardController');
 const { protect } = require('../middleware/authMiddleware');
 
-// حماية جميع المسارات في هذا الملف
-router.use(protect);
-
-// مسار عرض الصفحة الرئيسية
-router.get('/', getDashboardPage);
-
-// مسار إنشاء طلب جديد
-router.post('/create-order', createOrder); // <-- إضافة جديدة
+router.get('/', protect, dashboardController.getDashboard);
+router.post('/create-order', protect, dashboardController.createOrder);
 
 module.exports = router;
