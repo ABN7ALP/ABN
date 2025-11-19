4a83636e99bda93c778e41b4e2a4bed8b48559c8
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-11-20 02:59:38 +0300

    Update fundsRoutes.js

--- src/routes/fundsRoutes.js
+++ src/routes/fundsRoutes.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// =================== الإصلاح هنا ===================
-// استيراد كل الدوال المصدرة من وحدة التحكم
-const fundsController = require('../controllers/fundsController'); 
-// =================================================
-
+const fundsController = require('../controllers/fundsController');
 const { protect } = require('../middleware/authMiddleware');
 
-// حماية جميع مسارات شحن الرصيد (يجب أن يكون المستخدم مسجلاً دخوله)
 router.use(protect);
 
-// =================== والإصلاح هنا أيضاً ===================
-// استخدام الدوال من الكائن الذي تم استيراده
 router.get('/', fundsController.getFundsPage);
 router.post('/request', fundsController.createFundRequest);
-// ========================================================
 
 module.exports = router;
