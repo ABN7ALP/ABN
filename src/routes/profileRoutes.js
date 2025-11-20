376f2da915fa2d2eaf26cc77234fee1ee5af9bf6
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-11-20 04:05:16 +0300

    Update profileRoutes.js

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -8,5 +8,6 @@ router.use(protect);
 
 router.get('/', profileController.getProfilePage);
 router.post('/change-password', profileController.changePassword);
+router.post('/upload-image', profileController.uploadImage); // <-- السطر الجديد
 
 module.exports = router;
