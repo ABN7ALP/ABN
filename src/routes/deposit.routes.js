75083fa61e2a46c2368720b19beeeb7d8dd759b8
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-11-22 21:56:49 +0300

    Update deposit.routes.js

--- src/routes/deposit.routes.js
+++ src/routes/deposit.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user.model');
 const Deposit = require('../models/deposit.model');
 
 // POST /api/deposits - إنشاء طلب شحن جديد
