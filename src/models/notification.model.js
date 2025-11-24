07caecbc6818442d9c75bb400d2ab5235e5e9a52
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-11-25 00:13:42 +0300

    Create notification.model.js

---
+++ src/models/notification.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+    // لمن هذا الإشعار؟
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    // نص الإشعار
+    message: {
+        type: String,
+        required: true
+    },
+    // هل قرأ المستخدم الإشعار؟
+    read: {
+        type: Boolean,
+        default: false
+    },
+    // رابط يمكن الضغط عليه (اختياري، مثلاً رابط لصفحة "طلباتي")
+    link: {
+        type: String,
+        default: '#'
+    }
+}, {
+    timestamps: true // لإضافة تاريخ إنشاء الإشعار تلقائياً
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
