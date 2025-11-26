f471caaefda491b6b735017a15e730b872cfcf87
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-11-27 00:22:39 +0300

    Update emailService.js

--- src/routes/emailService.js
+++ src/routes/emailService.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 // إعداد transporter باستخدام Gmail
 const createTransporter = () => {
-    return nodemailer.createTransporter({
+    return nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: process.env.GMAIL_USER,
