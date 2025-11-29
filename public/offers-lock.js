4d290254226c5946cc7b4bf675f317443d466dca
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-11-30 02:14:10 +0300

    Update offers-lock.js

--- public/offers-lock.js
+++ public/offers-lock.js
@@ -9,7 +9,7 @@ class OffersLockSystem {
 
     checkDeveloperRole() {
         // 🔧 غير هذا الرقم إلى رقم المطور الحقيقي
-        const developerUserId = '65a1b2c3d4e5f67890123456'; 
+        const developerUserId = '6921d6a914a8ff08372c731a'; 
         const userInfo = JSON.parse(localStorage.getItem('userInfo'));
         
         return userInfo && userInfo._id === developerUserId;
