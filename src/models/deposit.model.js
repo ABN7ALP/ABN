55dc1650f8176ce700d401465e99dabf91bbe835
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-02 02:14:54 +0300

    Update deposit.model.js

--- src/models/deposit.model.js
+++ src/models/deposit.model.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const depositSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     amount: { type: Number, required: true },
-    method: { type: String, required: true, enum: ['bank', 'sham', 'whatsapp'] },
+    method: { type: String, required: true, enum: ['bank', 'sham', 'whatsapp', 'usdt', 'trx', 'bnb'] },
     depositorName: { type: String, required: true },
     
     // 🔄 التعديل هنا: تخزين رابط الصورة فقط
