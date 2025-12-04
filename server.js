70938ccbbb380d9785e8ed2cef758d20b2bdcbf6
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-04 14:17:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const {
 } = require('./src/middleware/rateLimit');
 
 const app = express();
+app.set('trust proxy', 1);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
