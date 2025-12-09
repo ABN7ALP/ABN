004d64db75e7ae3532aaefece5f3a0eda7650207
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-09 15:46:40 +0300

    Update order.model.js

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -38,11 +38,29 @@ const orderSchema = new mongoose.Schema({
         type: String,
         trim: true,
         default: null
+   },         
+     // 🚀🚀 الحقول الجديدة لنظام الاعتراض 🚀🚀
+    dispute: {
+        status: {
+            type: String,
+            enum: ['pending', 'approved', 'rejected', null], // pending: تم الاعتراض, approved: تمت الموافقة, rejected: تم الرفض
+            default: null
+        },
+        reason: { // سبب اعتراض المستخدم
+            type: String,
+            trim: true
+        },
+        adminResponse: { // رد الأدمن
+            type: String,
+            trim: true
+        },
+        date: { // تاريخ تقديم الاعتراض
+            type: Date
+        }
     }
 }, {
     timestamps: true 
 });
 
 const Order = mongoose.model('Order', orderSchema);
-
 module.exports = Order;
