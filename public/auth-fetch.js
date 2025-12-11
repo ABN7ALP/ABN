e6dc50ff3689225cbb729ff516a90c052c7649b9
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2025-12-11 04:07:04 +0300

    Update auth-fetch.js

--- public/auth-fetch.js
+++ public/auth-fetch.js
@@ -42,6 +42,10 @@ async function apiFetch(url, options = {}) {
         headers['Authorization'] = `Bearer ${authToken}`;
     }
 
+
+    const currentLng = localStorage.getItem('i18nextLng') || 'ar';
+    headers['Accept-Language'] = currentLng;
+
     // تحديد نوع المحتوى تلقائياً
     if (!(options.body instanceof FormData)) {
         headers['Content-Type'] = 'application/json';
