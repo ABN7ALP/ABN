e1d07cea589f2c583ec62e2d7045c525af1a8b0f
ABN7ALP <136245042+ABN7ALP@users.noreply.github.com>
2026-03-29 02:15:19 +0300

    Update PUBG image URL to new source

--- public/app.js
+++ public/app.js
@@ -1855,7 +1855,7 @@ function getPlatformIcon(platform, serviceName = '') {
 
      // --- 3.5 أيقونات الألعاب والتطبيقات ---
     else if (p.includes('pubg') || p.includes('ببجي')) {
-        originalUrl = 'https://upload.wikimedia.org/wikipedia/en/6/6b/PUBG_MOBILE.jpg';
+        originalUrl = 'https://ibb.co/ycG34kbJ"><img src="https://i.ibb.co/ycG34kbJ/image.png';
     }
     else if (p.includes('free fire') || p.includes('فري فاير') || p.includes('freefire')) {
         originalUrl = 'https://upload.wikimedia.org/wikipedia/en/6/6e/Free_Fire_logo.jpg';
